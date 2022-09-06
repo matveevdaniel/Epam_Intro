19eb80061ad5c2e8b01a196bac4f9085a9623847
Danik <Danik@HOME-PC>
2022-09-06 15:05:36 +0300

    update brancTask1

--- Epam_Intro_To_Java/src/by/epam/module1/branching/BranchingTask1.java
+++ Epam_Intro_To_Java/src/by/epam/module1/branching/BranchingTask1.java
@@ -3,15 +3,27 @@ package by.epam.module1.branching;
 public class BranchingTask1 {
 
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+
+	/*
+	 * Даны два угла треугольника (в градусах). Определить, существует ли такой
+	 * треугольник, и если да, то будет ли он прямоугольным.
+	 */
+
+	boolean isRightTriangle;
+
+	isRightTriangle = hasRightAngle(90, 50);
+
+	System.out.println("Является ли треугольник прямоугольным: " + isRightTriangle);
 
     }
-    
-    public static boolean isRightTriangle(int firstAngle,int secondAngle) {
-	    if((firstAngle+secondAngle<180)&((firstAngle!=0)&(secondAngle!=0))) 
-		if((firstAngle+secondAngle==90)||(firstAngle==90||secondAngle==90)) return true;
-	    
-	    return false;
-	}
 
+    public static boolean hasRightAngle(int firstAngle, int secondAngle) {
+
+	if ((firstAngle + secondAngle < 180) && ((firstAngle != 0) && (secondAngle != 0))) {
+	    if ((firstAngle + secondAngle == 90) || (firstAngle == 90 || secondAngle == 90)) {
+		return true;
+	    }
+	}
+	return false;
+    }
 }
